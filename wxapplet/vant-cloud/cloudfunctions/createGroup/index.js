fb352e0ad724dea887443fdc9d2e89080e49e98b
MoMaek-f <1034222074@qq.com>
2019-10-26 10:09:44 +0800

    云开发

---
+++ wxapplet/vant-cloud/cloudfunctions/createGroup/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const env ='ftl-l73vu'
+cloud.init()
+//获取数据库句柄
+  const db = cloud.database({env})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const userInfo = event.userInfo 
+  return await db.collection('group').add({
+    data:{
+      name:event.groupName,
+      createBy:userInfo.openid,
+      createTime:new Date(),
+      delete:false,
+      updateTime:new Date()
+    }
+  })
+}
