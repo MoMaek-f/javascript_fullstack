39a3a387f141200b08c29d4ea4236984cc8da67f
MoMaek-f <1034222074@qq.com>
2019-11-16 08:32:41 +0800

    vue

---
+++ vue/vue-communication/src/components/view/demo4/child1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="child">
+      <h3>兄弟组件1(监听兄弟组件2)</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/vue-communication/src/router/index.js
+++ vue/vue-communication/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import D1 from '@/components/view/demo1/father'
 import D2 from '@/components/view/demo2/father'
 import D3 from '@/components/view/demo3/father'
+import D4 from '@/components/view/demo3/father'
 
 Vue.use(Router)
 
@@ -23,5 +24,10 @@ export default new Router({
       name: 'D3',
       component: D3
     },
+    {
+      path: '/d4',
+      name: 'D4',
+      component: D4
+    }
   ]
 })
