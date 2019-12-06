068677ef35a8205f82ac039efcee5e185b361703
MoMaek-f <1034222074@qq.com>
2019-12-06 21:06:11 +0800

    vue

---
+++ nootBook/front/src/components/publishNote/publishNote.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="publish-note">
+    zz is sleeping
+    <quill-editor >
+    </quill-editor>
+  </div>
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import {quillEditor} from 'vue-quill-editor'
+export default {
+  name: 'publishNote',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    quillEditor
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ nootBook/noot_book/controllers/defaultConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  database: {
+      DATABASE: 'test',
+      USERNAME: 'root',
+      PASSWORD: '1234',
+      PORT: '3306',
+      HOST: 'localhost'
+  }
+}
+
+module.exports = config
