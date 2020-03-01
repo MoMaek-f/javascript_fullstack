4b2661cf920db832153ced0ce7f84c908f04315d
MoMaek-f <1034222074@qq.com>
2020-03-01 21:36:19 +0800

    react初试

--- react/learn/src/index.js
+++ react/learn/src/index.js
@@ -1,51 +1,12 @@
-//r入口
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import App from './App'
-// class App extends React.Component {
-//   render() {
-//   return <div>hello {this.props.name},i am {9+9}</div>
-//   }
-// }
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-//state
-class Counter extends React.Component {
-  constructor (props) {
-    super(props)
-    //初始化构造器时设置内部状态 num值为1
-    this.state = {
-      num: 1
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-  //生命周期函数，组件渲染完成
-  componentDidMount() {
-    console.log('componentDidMount 函数触发')
-  }
-  //生命周期函数 避免组件无意义或重复的渲染
-  shouldComponentUpdate(nextProps,nextState){
-        // console.log(nextProps,nextState)
-        if (nextState.num%2) {
-          return true
-        }
-        return false
-  }
-  handleClick() {
-    console.log(123)
-    this.setState ({
-      num: this.state.num +1
-    })
-  }
-  render () {
-    return (
-      <div>
-        <p>{this.state.num}</p>
-        <button onClick={this.handleClick}>
-          click
-        </button>
-      </div>
-    )
-  }
-}
-ReactDOM.render(<Counter/>, document.querySelector('#root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
